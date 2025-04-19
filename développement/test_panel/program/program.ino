b6a3496788d5c3efaf6e40a565c170da2673cb5d
Σlie * <ELIE.DELETANG.LEGRAVIER@GMAIL.COM>
2025-04-19 15:46:35 +0200

    add potentiometer and switch support

--- développement/test_panel/program/program.ino
+++ développement/test_panel/program/program.ino
@@ -285,9 +285,107 @@ void joystickLiveView() {
   drawMenu(selectedOption);  // Retour au menu après avoir quitté
 }
 
+void potentiometerLiveView() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor(20, 10);
+  tft.println("Potentiometer Live");
+
+  // Jauge X - barre horizontale
+  tft.setCursor(10, 50);
+  tft.print("LP");
+  tft.drawRect(40, 50, 160, 10, ST77XX_WHITE);  // Jauge X horizontale
+
+  // Jauge Y - barre horizontale
+  tft.setCursor(10, 80);
+  tft.print("RP");
+  tft.drawRect(40, 80, 160, 10, ST77XX_WHITE);  // Jauge Y horizontale
+
+  // Variables pour afficher les valeurs
+  int lastX = -1;
+  int lastY = -1;
+  int lastZ = -1;
+  unsigned long buttonPressStart = 0;
+  bool buttonHeld = false;
+  bool inJoystickView = true;  // Variable pour éviter de retourner immédiatement
+
+  // Variables pour gérer l'affichage des valeurs
+  int lastXVal = -1;
+  int lastYVal = -1;
+
+  while (inJoystickView) {
+    int xVal = analogRead(15);
+    int yVal = 4095-analogRead(0);
+    int zVal = digitalRead(xyzPins1[2]);
+
+    int xBarLength = map(xVal, 0, 4095, 0, 160);  // Taille de la barre X
+    int yBarLength = map(yVal, 0, 4095, 0, 160);  // Taille de la barre Y
+
+    // Mise à jour de la jauge X
+    if (xBarLength != lastX) {
+      tft.fillRect(40, 50, 160, 10, ST77XX_BLACK);  // Efface l'ancienne jauge
+      tft.fillRect(40, 50, xBarLength, 10, ST77XX_RED);  // Nouvelle jauge
+      lastX = xBarLength;
+    }
+
+    // Mise à jour de la jauge Y
+    if (yBarLength != lastY) {
+      tft.fillRect(40, 80, 160, 10, ST77XX_BLACK);  // Efface l'ancienne jauge
+      tft.fillRect(40, 80, yBarLength, 10, ST77XX_GREEN);  // Nouvelle jauge
+      lastY = yBarLength;
+    }
+
+    // Affichage de l'état du bouton
+    if (zVal != lastZ) {
+      tft.fillRect(70, 180, 160, 20, ST77XX_BLACK);  // Effacer la zone du bouton
+      tft.setCursor(70, 180);
+      tft.setTextColor(zVal == LOW ? ST77XX_GREEN : ST77XX_WHITE);
+      tft.print("exit (press 1s)");
+      lastZ = zVal;
+    }
+
+    // Effacer les anciennes valeurs X et Y
+    if (xVal != lastXVal) {
+      tft.fillRect(210, 50, 50, 20, ST77XX_BLACK);  // Efface l'ancienne valeur X
+      tft.setCursor(210, 50);
+      tft.print(xVal);  // Affichage de la nouvelle valeur X
+      lastXVal = xVal;
+    }
+
+    if (yVal != lastYVal) {
+      tft.fillRect(210, 80, 50, 20, ST77XX_BLACK);  // Efface l'ancienne valeur Y
+      tft.setCursor(210, 80);
+      tft.print(yVal);  // Affichage de la nouvelle valeur Y
+      lastYVal = yVal;
+    }
+
+    // Détection d'appui long pour quitter
+    if (zVal == LOW) {
+      if (!buttonHeld) {
+        buttonPressStart = millis();
+        buttonHeld = true;
+      } else if (millis() - buttonPressStart >= 1000) {  // Appui long (1 seconde)
+        inJoystickView = false;  // Quitter la vue joystick
+        playMenuSound();  // Son lors du changement d'option
+        delay(500);
+      }
+    } else {
+      buttonHeld = false;  // Réinitialisation si on relâche le bouton
+    }
+
+    delay(50);
+  }
+
+  drawMenu(selectedOption);  // Retour au menu après avoir quitté
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(buzzerPin, OUTPUT);
+  pinMode(15, INPUT);
+  pinMode(0, INPUT);  
+  pinMode(32, INPUT_PULLUP);
   pinMode(xyzPins1[2], INPUT_PULLUP);
   tft.init(240, 320);
   tft.setRotation(1);
@@ -299,9 +397,14 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(digitalRead(32));
+  delay(200);
+
   int xVal = analogRead(xyzPins1[0]);
   int yVal = analogRead(xyzPins1[1]);
   int zVal = digitalRead(xyzPins1[2]);
+
+
 
   // Joystick en haut
   if (yVal > 3000) {
@@ -327,6 +430,9 @@ void loop() {
         break;
       case 1:
         joystickLiveView();
+        break;
+      case 2:
+        potentiometerLiveView();
         break;
       case 3:
         soundSettingsMenu();
